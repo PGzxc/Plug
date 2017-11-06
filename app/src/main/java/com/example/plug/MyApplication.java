bd281609c05efc9b7e54fd198cf4ab63aea6f38c
827489398@qq.com <Zxc19871107>
2017-11-06 17:28:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/plug/MyApplication.java
@@ -0,0 +1,26 @@
+package com.example.plug;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.morgoo.droidplugin.PluginHelper;
+
+/**
+ * Created by Admin on 2017/11/5.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //这里必须在super.onCreate方法之后，顺序不能变
+        PluginHelper.getInstance().applicationOnCreate(getBaseContext());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        PluginHelper.getInstance().applicationAttachBaseContext(base);
+        super.attachBaseContext(base);
+    }
+}
